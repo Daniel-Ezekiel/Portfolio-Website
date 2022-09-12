e4109e539dfb39bb25240cd0feec091b9c3405a7
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-09-13 00:21:53 +0100

    optimized general code and fixed errors in the html, css and js code bases

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-const menuBtn = document.querySelector('.hamburger').addEventListener('click', showMenu);
+const menuBtn = document.querySelector('.hamburger');
 const menuBar = document.querySelector('.menu-bar');
-const menuList = document.querySelector('.nav-menu')
+const menuList = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', showMenu);
 
 function showMenu(){
+    menuBtn.classList.toggle('is-active');
     menuBar.classList.toggle('is-active');
     menuList.classList.toggle('is-active');
 }
